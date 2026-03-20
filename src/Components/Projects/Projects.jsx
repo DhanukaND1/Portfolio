aae3f7f45cd24fcbe9d755a3107110ad6603c072
DhanukaND1 <2022t01555@stu.cmb.ac.lk>
2026-03-20 21:23:54 +0530

    Fix image importing issues

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 import project1 from "../../assets/project1.png";
 import project2 from "../../assets/project2.png";
 import project3 from "../../assets/project3.png";
-import project4 from "../../assets/project3.png";
-import project5 from "../../assets/project3.png";
+import project4 from "../../assets/project4.png";
+import project5 from "../../assets/project5.png";
 import "./Projects.css";
 
 const projectList = [
